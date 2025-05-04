8a6d6d93809a5a96d3c179e21eb839fede49dc06
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-05-04 18:36:12 +0600

    recipe cart done

--- chefs-table/src/components/recipes/Recipe.jsx
+++ chefs-table/src/components/recipes/Recipe.jsx
@@ -1,15 +1,17 @@
 import propTypes from "prop-types";
+import { CiStopwatch } from "react-icons/ci";
+import { AiOutlineFire } from "react-icons/ai";
 
 function Recipe({recipe}) {
   return (
     <div className='our-recipes p-6 flex flex-col gap-4 justify-center items-center bg-white rounded-lg border-1 border-gray-200'> 
-      <img className="h-48 rounded-2xl" src={recipe.image} alt="" />
+      <img className="h-48 rounded-2xl w-full" src={recipe.image} alt="" />
 
       <div className="flex flex-col gap-4"> 
         <h2 className="text-xl">{recipe.recipe_name}</h2>
         <p>{recipe.short_description}</p>
         <hr className="text-gray-300" />
-        <h3>Ingredients: {recipe.ingredients.length}</h3>
+        <h3 className="text-lg font-medium">Ingredients: {recipe.ingredients.length}</h3>
 
         <ul className="list-disc ml-12 text-gray-500">
             {
@@ -18,6 +20,22 @@ function Recipe({recipe}) {
                 </li>)
             }
         </ul>
+
+        <hr className="text-gray-300" />
+
+        <div className="flex gap-4 justify-start items-center">
+            <div className="flex gap-2 justify-center items-center">
+                <CiStopwatch />
+                <p>{recipe.preparing_time}</p>
+            </div>
+
+            <div className="flex gap-2 justify-center items-center">
+                <AiOutlineFire />
+                <p>{recipe.calories}</p>
+            </div>
+        </div>
+
+        <button className="button w-fit">Want to Cook</button>
       </div>
     </div>
   )
