33fe74f311a11a76d31bda0479247604f6d49be9
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-05-04 17:26:17 +0600

    banner done

--- chefs-table/src/components/header/Header.jsx
+++ chefs-table/src/components/header/Header.jsx
@@ -27,13 +27,17 @@ function Header() {
       </nav>
 
       {/* banner */}
-      <div className="banner h-[600px] bg-[url('chef.jpg')] bg-cover flex flex-col justify-center items-center px-40 text-center">
-        <h1>Discover an exceptional cooking class tailored for you!</h1>
-        <p>Learn and master the essentials of culinary artistry — from fundamental techniques and flavor-building to recipe structuring, plating, and kitchen management.</p>
-        
-        <div>
-          <button className="button">Explore Now</button>
-          <button className="button">Our Feedback</button>
+      <div className="banner h-[600px] bg-[url('chef.jpg')] bg-cover flex flex-col justify-center items-center px-40 text-center gap-10 relative rounded-3xl">
+        <div className="absolute z-2 bg-black opacity-50 w-full h-full rounded-3xl"></div>
+
+        <div className="z-10 flex flex-col gap-10 text-white justify-center items-center">
+          <h1>Discover an exceptional cooking class tailored for you!</h1>
+          <p>Learn and master the essentials of culinary artistry — from fundamental techniques and flavor-building to recipe structuring, plating, and kitchen management.</p>
+          
+          <div className="flex gap-6">
+            <button className="button">Explore Now</button>
+            <button className="text-white text-xl font-semibold border-2 px-6 py-4 rounded-4xl">Our Feedback</button>
+          </div>
         </div>
       </div>
     </div>
