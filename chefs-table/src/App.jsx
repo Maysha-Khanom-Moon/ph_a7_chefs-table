b486bb83284cd1e72bf6e06c79b1d5a696516e7e
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-05-04 17:52:09 +0600

    Our Recipes sections' structure done

--- chefs-table/src/App.jsx
+++ chefs-table/src/App.jsx
@@ -1,12 +1,21 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 import Header from './components/header/Header';
 import OurRecipes from './components/recipes/OurRecipes';
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetch('recipes.json')
+      .then(res => res.json())
+      .then(data => setRecipes(data))
+  }, []);
+
   return (
     <div className='px-36 pt-10 pb-24'>
       <Header></Header>
-      <OurRecipes></OurRecipes>
+      <OurRecipes recipes={recipes}></OurRecipes>
     </div>
   )
 }
--- chefs-table/src/components/recipes/OurRecipes.jsx
+++ chefs-table/src/components/recipes/OurRecipes.jsx
@@ -1,10 +1,31 @@
-function OurRecipes() {
+import propTypes from "prop-types";
+import Recipe from "./Recipe";
+
+function OurRecipes({recipes}) {
   return (
-    <div className="our-recipes flex flex-col gap-4 justify-center items-center text-center mt-20 mb-12">
-      <h1>Our Recipes</h1>
-      <p className="w-[823px]">Discover delicious cooking recipes from around the world. From hearty comfort food to healthy dishes, learn step-by-step techniques to create meals that impress. Master flavors.</p>
+    <div>
+      <div className="our-recipes flex flex-col gap-4 justify-center items-center text-center mt-20 mb-12">
+        <h1>Our Recipes</h1>
+        <p className="w-[823px]">Discover delicious cooking recipes from around the world. From hearty comfort food to healthy dishes, learn step-by-step techniques to create meals that impress. Master flavors.</p>
+      </div>
+
+      <div>
+        <div className="w-2/3">
+          {
+            recipes.map(recipe => <Recipe key={recipe.recipe_id} recipe={recipe}></Recipe>)
+          }
+        </div>
+
+        <div className="w-1/3">
+
+        </div>
+      </div>
     </div>
   )
+}
+
+OurRecipes.PropTypes = {
+  recipes: propTypes.array.isRequired
 }
 
 export default OurRecipes
