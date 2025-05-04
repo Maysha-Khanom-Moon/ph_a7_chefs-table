40ebc35f6d58735b14658ad4c26d127d99762ffa
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-05-04 16:44:02 +0600

    navbar done

--- chefs-table/src/App.jsx
+++ chefs-table/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Header from './components/header/Header';
 
 function App() {
   return (
-    <>
+    <div className='px-36 pt-10 pb-24'>
+      <Header></Header>
       
-    </>
+    </div>
   )
 }
 
--- chefs-table/src/components/header/Header.jsx
+++ chefs-table/src/components/header/Header.jsx
@@ -1,11 +1,11 @@
 import { IoSearchOutline } from "react-icons/io5";
-import { FaRegUserCircle } from "react-icons/fa";
+import { LuCircleUserRound } from "react-icons/lu";
 
 function Header() {
   return (
-    <div>
+    <div> 
       {/* navbar */}
-      <nav>
+      <nav className="flex justify-between items-center mb-10">
             <h1>Recipe Calories</h1>
 
             <ul className="flex gap-4">
@@ -15,13 +15,13 @@ function Header() {
                 <li className="text-lg font-semibold">Search</li>
             </ul>
 
-            <div>
-                <div>
-                    <button><IoSearchOutline /></button>
-                    <input type="text" placeholder="Search..." className="border-2 border-gray-300 rounded-md px-4 py-2" />
+            <div className="flex items-center gap-4">
+                <div className="flex items-center bg-[#150B2B0D] px-4 py-2 text-2xl font-normal gap-2 rounded-2xl">
+                    <button className="p-1 cursor-pointer"><IoSearchOutline /></button>
+                    <input type="text" placeholder="Search..." className="rounded-md px-4 py-1 w-60" />
                 </div>
-                <div>
-                  <FaRegUserCircle />
+                <div className="bg-[#0BE58A] p-2 rounded-full w-14 h-14 flex items-center justify-center">
+                  <LuCircleUserRound />
                 </div>
             </div>
       </nav>
@@ -30,4 +30,3 @@ function Header() {
 }
 
 export default Header
-
