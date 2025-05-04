acaac9df1d97d16b4f5946ef60e7e926ab0cb289
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-05-04 17:42:21 +0600

    Our Recipes section done

--- chefs-table/src/App.jsx
+++ chefs-table/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Header from './components/header/Header';
+import OurRecipes from './components/recipes/OurRecipes';
 
 function App() {
   return (
     <div className='px-36 pt-10 pb-24'>
       <Header></Header>
-      
+      <OurRecipes></OurRecipes>
     </div>
   )
 }
---
+++ chefs-table/src/components/recipes/OurRecipes.jsx
@@ -0,0 +1,10 @@
+function OurRecipes() {
+  return (
+    <div className="our-recipes flex flex-col gap-4 justify-center items-center text-center mt-20 mb-12">
+      <h1>Our Recipes</h1>
+      <p className="w-[823px]">Discover delicious cooking recipes from around the world. From hearty comfort food to healthy dishes, learn step-by-step techniques to create meals that impress. Master flavors.</p>
+    </div>
+  )
+}
+
+export default OurRecipes
